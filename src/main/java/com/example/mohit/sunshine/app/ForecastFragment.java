fe3d097e259b9b0a5913812b666f81c94aee3211
itsmohitgoel <itsmohitgoel@gmail.com>
2016-10-04 11:42:46 +0530

    feat: Add EmptyView for ListView with no data

--- src/main/java/com/example/mohit/sunshine/app/ForecastFragment.java
+++ src/main/java/com/example/mohit/sunshine/app/ForecastFragment.java
@@ -126,6 +126,8 @@ public class ForecastFragment extends Fragment implements Updatable, LoaderManag
         mForecastAdapter = new ForecastAdapter(getActivity(), null, 0);
 
         mListView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        View emptyView = (View) rootView.findViewById(R.id.listview_forecast_empty);
+        mListView.setEmptyView(emptyView);
         mListView.setAdapter(mForecastAdapter);
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
